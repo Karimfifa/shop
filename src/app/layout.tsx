bd64d7e229c0581915e8ce1debb7d7e97fbcf6b9
karimfifa <fifakarim52@gmail.com>
2024-04-04 04:01:39 +0000

    hahah

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from '@clerk/nextjs'
  
 
 
@@ -18,11 +17,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
       <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-    </ClerkProvider>
     
   );
 }
